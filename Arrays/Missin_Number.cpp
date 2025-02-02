5e27a60aa5613085a1115af4594ce20f25604bf8
dhawall-s <sainidhawal755@gmail.com>
2025-02-02 15:35:53 +0530

    new update

---
+++ Arrays/Missin_Number.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int arr[]={1,3,4,5};
+    int size;
+    int arr[100];
+    cin>>size;
+    size++;
+    int sum =size*(size+1)/2;
+    cout<<"Sum of the N natural Numbers are :"<<sum<<endl;
+    int sum1=0;
+    for(int i=0;i<size-1;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<size-1;i++){
+        sum1+=arr[i];
+    }
+    cout<<"Missing Number is"<<sum-sum1;
+
+
+}
--- Arrays/Reverse_Of_Array.cpp
+++ Arrays/Reverse_Of_Array.cpp
@@ -20,6 +20,4 @@ int main(){
     for(int i=0;i<size;i++){
         cout<<" "<<arr[i];
     }
-
-
 }

faaee4d9a632382312c7dfbe90734ca308407258
dhawall-s <sainidhawal755@gmail.com>
2025-02-13 22:34:17 +0530

    new update

--- POTD/Level_order_traversal.cpp
+++ POTD/Level_order_traversal.cpp
@@ -61,11 +61,7 @@ class Solution {
     }
 };
 
-//{ Driver Code Starts.
 
-/* Helper function to test mirror(). Given a binary
-   search tree, print out its data elements in
-   increasing sorted order.*/
 void inOrder(Node* node) {
     if (node == nullptr)
         return;
@@ -76,61 +72,45 @@ void inOrder(Node* node) {
     inOrder(node->right);
 }
 
-// Function to Build Tree
 Node* buildTree(string str) {
-    // Corner Case
     if (str.length() == 0 || str[0] == 'N')
         return nullptr;
 
-    // Creating vector of strings from input
-    // string after spliting by space
     vector<string> ip;
 
     istringstream iss(str);
     for (string str; iss >> str;)
         ip.push_back(str);
 
-    // Create the root of the tree
     Node* root = newNode(stoi(ip[0]));
 
-    // Push the root to the queue
     queue<Node*> queue;
     queue.push(root);
 
-    // Starting from the second element
     int i = 1;
     while (!queue.empty() && i < ip.size()) {
 
-        // Get and remove the front of the queue
         Node* currNode = queue.front();
         queue.pop();
 
-        // Get the current node's value from the string
         string currVal = ip[i];
 
-        // If the left child is not null
         if (currVal != "N") {
 
-            // Create the left child for the current node
             currNode->left = newNode(stoi(currVal));
 
-            // Push it to the queue
             queue.push(currNode->left);
         }
 
-        // For the right child
         i++;
         if (i >= ip.size())
             break;
         currVal = ip[i];
 
-        // If the right child is not null
         if (currVal != "N") {
 
-            // Create the right child for the current node
             currNode->right = newNode(stoi(currVal));
 
-            // Push it to the queue
             queue.push(currNode->right);
         }
         i++;
@@ -139,7 +119,6 @@ Node* buildTree(string str) {
     return root;
 }
 
-/* Driver program to test size function*/
 int main() {
     int t;
     scanf("%d ", &t);
@@ -159,4 +138,3 @@ int main() {
     return 0;
 }
 
-// } Driver Code Ends

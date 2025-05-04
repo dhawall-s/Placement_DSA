91f3e048081ef52719d17bb9f008f09d9a4aed94
dhawall-s <sainidhawal755@gmail.com>
2025-05-04 21:23:19 +0530

    Initial commit

---
+++ POTD/numEquivDominoPairs.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    int numEquivDominoPairs(vector<vector<int>>& dominoes) {
+        unordered_map<int, int> freq;
+        int count = 0;
+        
+        for (auto& d : dominoes) {
+            int key = min(d[0], d[1]) * 10 + max(d[0], d[1]);
+            count += freq[key];
+            freq[key]++;
+        }
+        
+        return count;
+    }
+};
+
+int main() {
+    Solution sol;
+    
+    vector<vector<int>> dominoes1 = {{1,2},{2,1},{3,4},{5,6}};
+    vector<vector<int>> dominoes2 = {{1,2},{1,2},{1,1},{1,2},{2,2}};
+    
+    cout << "Output 1: " << sol.numEquivDominoPairs(dominoes1) << endl; // Expected: 1
+    cout << "Output 2: " << sol.numEquivDominoPairs(dominoes2) << endl; // Expected: 3
+
+    return 0;
+}

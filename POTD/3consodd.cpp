43dc7beef7a6aa17f486fba24a58ba7ccec2082f
dhawall-s <sainidhawal755@gmail.com>
2025-05-11 12:50:22 +0530

    Initial commit

---
+++ POTD/3consodd.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int count = 0;
+        for (int num : arr) {
+            if (num % 2 == 1) {
+                count++;
+                if (count == 3) return true;
+            } else {
+                count = 0;
+            }
+        }
+        return false;
+    }
+};
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+
+    Solution sol;
+    cout << (sol.threeConsecutiveOdds(arr) ? "true" : "false") << endl;
+    return 0;
+}
